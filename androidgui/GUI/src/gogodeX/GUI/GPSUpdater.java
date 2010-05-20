f3ae19698ee3515f8071eacdbd41506b5f73351f
Brian Garfinkel <bagrm@ucla.edu>
2010-05-19 17:03:58 -0700

    Integrated GUI with backend. Experimental and untested.

---
+++ androidgui/GUI/src/gogodeX/GUI/GPSUpdater.java
@@ -0,0 +1,121 @@
+package gogodeX.GUI;
+
+import java.io.IOException;
+import java.net.*;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.TextView;
+import org.json.*;
+
+
+public class GPSUpdater extends Service {
+
+	//LocationManager to update GPS coordinates
+	private static LocationManager LM = null;
+	//TextView for debug printing
+	private TextView TV = null;
+	//Static activity instance for activity data access
+	private static GUI gui;
+	//JavaClient to connect to the twisted server
+	private static JavaClient client;
+	//private RspHandler handler;
+	private Thread t;
+	//JSONStringer to parse a string into a json string
+	private JSONStringer coordinates;
+	//Holds the user's current Location
+	private Location location;
+	//Variables for holding latitude and longitude values
+	private static double latitude;
+	private static double longitude;
+	//Handler for handling incoming server messages
+	private MessageHandler serverUpdates;
+	
+	@Override
+	public void onCreate()
+	{
+		//Get the LocationManager and TextView from the main activity
+		GUI.getClient(client);
+		GUI.getLM(LM);
+		//Start the main GPS updating loop
+		GPSHandler();
+	}
+	
+	private void GPSHandler()
+	{
+		//Obtain the constant string value for the GPS provider
+	    String mocLocationProvider = LocationManager.GPS_PROVIDER;
+	    //Create a new mocLocationListener instance to handle coordinate updates
+	    LocationListener mocLocation = new mocLocationListener(); 
+	    //Initiate the updating of coordinates through the requestLocationUpdates method
+	    LM.requestLocationUpdates(mocLocationProvider, 0, 0, mocLocation);
+	    //Get the last known location of the device for the user's starting location
+	    location = LM.getLastKnownLocation(mocLocationProvider);
+	    
+		serverUpdates = new MessageHandler(client, this);
+	    t = new Thread(serverUpdates);
+	    t.start();
+	}
+	
+	//Class to handle GPS updates
+	private class mocLocationListener implements LocationListener 
+	{ 
+	            public void onLocationChanged(Location L) { 
+	            	//Set the user's location to the newly received location
+	            	location = L;
+	            	//Create a new JSONStringer to create a JSON string to send to the server
+	            	coordinates = new JSONStringer();
+	            		//As long as the new location was received
+	                    if (L != null) 
+	                    { 
+	                    	//Get the latitude and longitude from the current user's location
+	                    	latitude = L.getLatitude(); 
+	                    	longitude = L.getLongitude();          
+	                    	try 
+	                    	{                		
+	                    		//Create a new JSON object
+	                    		coordinates.object();
+	                    		//Add the keys needed for updating a user's coordinates
+								coordinates.key("Lon").value(longitude);
+		                    	coordinates.key("Lat").value(latitude);
+		                    	coordinates.key("Request Type").value("Update Coordinate");
+		                    	//End the JSON object
+		                    	coordinates.endObject();
+							} 
+	                    	catch (JSONException e) 
+	                    	{
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+	                    	TV.append(coordinates.toString());
+	                    	//Send the parsed JSON update coordinates request
+								client.sendLine(coordinates.toString());
+	                    } 
+	                    else 
+	                    {
+	                    	TV.append("Bad Provider\n"); 
+	                    } 
+	            } 
+	            public void onProviderDisabled(String provider) { 
+	                    // TODO Auto-generated method stub 
+	            } 
+	            public void onProviderEnabled(String provider) { 
+	                    // TODO Auto-generated method stub 
+	            } 
+	            public void onStatusChanged(String provider, int status, Bundle extras) { 
+	                    // TODO Auto-generated method stub 
+	            } 
+		}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- androidgui/GUI/src/gogodeX/GUI/GUI.java
+++ androidgui/GUI/src/gogodeX/GUI/GUI.java
@@ -3,9 +3,16 @@ package gogodeX.GUI;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import gogodeX.GUI.GPSUpdater;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
@@ -15,11 +22,15 @@ import android.widget.Toast;
 
 public class GUI extends Activity {
     /** Called when the activity is first created. */
-	private JavaClient client;
+	private static JavaClient client;
 	private String user;
 	private String pass;
 	private Editable userEd;
 	private Editable passEd;
+	private JSONStringer JSONUser;
+	private static LocationManager LM;
+	private JSONObject JSONValidate;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +41,10 @@ public class GUI extends Activity {
         final EditText password = (EditText) findViewById(R.id.password);
         final Context context = getApplicationContext();
         final int duration = Toast.LENGTH_SHORT;
+        final Intent startUpdatingCoordinates = new Intent(this, GPSUpdater.class);
         
-        client = new JavaClient("127.0.0.1", 79);
+        client = new JavaClient("169.232.101.67", 79);
+        LM = (LocationManager) getSystemService(Context.LOCATION_SERVICE); 
         
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -48,11 +61,18 @@ public class GUI extends Activity {
             			Toast.makeText(context, text, duration).show();
             			return;
             		}
-            		else
+            		boolean validated = validateUser();
+            		if(validated == false)
             		{
-            			Intent myIntent = new Intent(view.getContext(), Tabs.class);
-            			startActivity(myIntent);
+            			CharSequence text = "Sorry, User Name and/or Password not Found!";
+            			Toast.makeText(context, text, duration).show();
+            			return;	
             		}
+            		
+            	    startService(startUpdatingCoordinates);
+            	    
+            		Intent myIntent = new Intent(view.getContext(), Tabs.class);
+            		startActivity(myIntent);
             	}
             	else
             	{
@@ -64,7 +84,8 @@ public class GUI extends Activity {
          });    
       }
     
-    private boolean connectToServer(){
+    private boolean connectToServer()
+    {
     	try 
     	{
 			client.connect();
@@ -78,5 +99,50 @@ public class GUI extends Activity {
 		}
 		return true;
     }
+    
+    private boolean validateUser()
+    {
+    	JSONUser = new JSONStringer();
+    	try 
+    	{
+    		JSONUser.object();
+			JSONUser.key("User Name").value(userEd.toString());
+			JSONUser.key("Password").value(passEd.toString());
+			JSONUser.key("Request Type").value("Login");
+			JSONUser.endObject();
+			client.sendLine(JSONUser.toString());
+			String response = client.readLine();
+			JSONValidate = new JSONObject(response);
+			if(JSONValidate.get("Response Type") == "User Validation" && JSONValidate.getBoolean("Success") == true)
+			{
+				return true;	
+			}
+			else
+			{
+				return false;
+			}	
+		} 
+    	catch (JSONException e) 
+		{
+    		return false;			
+		}
+    	catch (IOException e)
+    	{
+    		e.printStackTrace();
+    		return false;
+    	}
+    	
+    }
+    
+    public static void getClient(JavaClient client)
+    {
+    	client = GUI.client;
+    }
+    
+    public static void getLM(LocationManager LM)
+    {
+    	LM = GUI.LM;
+    }
+    
 
 }
