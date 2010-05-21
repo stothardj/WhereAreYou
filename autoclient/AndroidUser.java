076152508830607f5ffb6f6365920af9dfce8a43
root <root@yijie-laptop.(none)>
2010-05-21 15:36:08 -0700

    added the JSON portion of AndroidUser, under autoclient

--- autoclient/AndroidUser.java
+++ autoclient/AndroidUser.java
@@ -15,6 +15,71 @@ public class AndroidUser {
 		}
 	}
 	
+	// JSON API
+
+	public void creatUser(String first_name, String last_name, String password, String account_type) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Create User", JSONType.STRING));
+		f.add(new JSONField("First Name", first_name, JSONType.STRING));
+		f.add(new JSONField("Last Name", last_name, JSONType.STRING));
+		f.add(new JSONField("User Name", username, JSONType.STRING));
+		f.add(new JSONField("Password", password, JSONType.STRING));
+		f.add(new JSONField("Account Type", account_type, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void removeUser() {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Remove User", JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void addZone(String zone_name, String lat, String lon, String radius) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Add Zone", JSONType.STRING));
+		f.add(new JSONField("Zone Name", zone_name, JSONType.STRING));
+		f.add(new JSONField("Lat", lat, JSONType.STRING));
+		f.add(new JSONField("Lon", lon, JSONType.STRING));
+		f.add(new JSONField("Radius", radius, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void removeZone(String zone_name) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Remove Zone", JSONType.STRING));
+		f.add(new JSONField("Zone Name", zone_name, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void addFriend(String friend_name) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Add Friend", JSONType.STRING));
+		f.add(new JSONField("Friend Name", friend_name, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void acceptFriend(String friend_name) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Accept Friend", JSONType.STRING));
+		f.add(new JSONField("Friend Name", friend_name, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void removeFriend(String friend_name) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Remove Friend", JSONType.STRING));
+		f.add(new JSONField("Friend Name", friend_name, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void updateCoordinate(String lat, String lon) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Update Coordinate", JSONType.STRING));
+		f.add(new JSONField("Lat", lat, JSONType.STRING));
+		f.add(new JSONField("Lon", lon, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
 	public void login(String password) {
 		LinkedList<JSONField> f = new LinkedList<JSONField>();
 		f.add(new JSONField("Request Type", "Login", JSONType.STRING));
@@ -22,8 +87,52 @@ public class AndroidUser {
 		f.add(new JSONField("Password", password, JSONType.STRING));
 		sendLine(AndroidUser.createJSONString(f));		
 	}
-	
-	//TODO: Following example of login, create functions for each thing a user can do
+
+	// JSON API (Testing only)
+	public void showUsers() {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Show Users", JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void showFriends() {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Show Friends", JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void showZones() {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Request Type", "Show Zones", JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	// server response JSON
+
+	public void userValidation(String success) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Response Type", "User Validation", JSONType.STRING));
+		f.add(new JSONField("Success", success, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void positionUpdate(String lat, String lon) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Response Type", "Position Update", JSONType.STRING));
+		f.add(new JSONField("User Name", username, JSONType.STRING));
+		f.add(new JSONField("Lat", lat, JSONType.STRING));
+		f.add(new JSONField("Lon", lon, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+	public void friendRequest(String from_user) {
+		LinkedList<JSONField> f = new LinkedList<JSONField>();
+		f.add(new JSONField("Response Type", "Friend Request", JSONType.STRING));
+		f.add(new JSONField("From User", from_user, JSONType.STRING));
+		sendLine(AndroidUser.createJSONString(f));		
+	}
+
+
 	
 	private void sendLine(String message) {
 		System.out.println(username+": "+message);
