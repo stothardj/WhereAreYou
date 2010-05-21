da19e9add972b5def1cc40f3e6a92efea858a03e
unknown <Hong@.(none)>
2010-05-21 01:45:45 -0700

    Added polling frequency on settings page

--- androidgui/GUI/gen/gogodeX/GUI/R.java
+++ androidgui/GUI/gen/gogodeX/GUI/R.java
@@ -8,18 +8,22 @@
 package gogodeX.GUI;
 
 public final class R {
+    public static final class array {
+        public static final int entries_polling=0x7f050000;
+        public static final int entryvalues_polling=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int mapview=0x7f060005;
-        public static final int ok=0x7f060004;
-        public static final int password=0x7f060003;
-        public static final int password_label=0x7f060002;
-        public static final int username=0x7f060001;
-        public static final int username_label=0x7f060000;
+        public static final int mapview=0x7f070005;
+        public static final int ok=0x7f070004;
+        public static final int password=0x7f070003;
+        public static final int password_label=0x7f070002;
+        public static final int username=0x7f070001;
+        public static final int username_label=0x7f070000;
     }
     public static final class layout {
         public static final int login=0x7f030000;
@@ -27,15 +31,17 @@ public final class R {
         public static final int maptabactivityview=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int login_button=0x7f050006;
-        public static final int mapActivity_name=0x7f050001;
-        public static final int password_prompt=0x7f050005;
-        public static final int settingActivity_name=0x7f050002;
-        public static final int summary_show_hide_toggle=0x7f050008;
-        public static final int tabs_name=0x7f050003;
-        public static final int title_show_hide_toggle=0x7f050007;
-        public static final int username_prompt=0x7f050004;
+        public static final int app_name=0x7f060000;
+        public static final int login_button=0x7f060006;
+        public static final int mapActivity_name=0x7f060001;
+        public static final int password_prompt=0x7f060005;
+        public static final int settingActivity_name=0x7f060002;
+        public static final int summary_polling=0x7f06000a;
+        public static final int summary_show_hide_toggle=0x7f060008;
+        public static final int tabs_name=0x7f060003;
+        public static final int title_polling=0x7f060009;
+        public static final int title_show_hide_toggle=0x7f060007;
+        public static final int username_prompt=0x7f060004;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
