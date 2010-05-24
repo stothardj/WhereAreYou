8cc88b73538e2bbfbe4800719db26336b0324f34
Adam <xzy1243@gmail.com>
2010-05-23 21:41:01 -0700

    Made the create user page send you back to the login screen after you send the create new account request

--- androidgui/GUI/src/gogodeX/GUI/Createaccnt.java
+++ androidgui/GUI/src/gogodeX/GUI/Createaccnt.java
@@ -1,15 +1,30 @@
 package gogodeX.GUI;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Createaccnt extends Activity{
 	 /** Called when the activity is first created. */
-    @Override
+
+    
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createaccount);
-    }
+    
+	
+    Button next = (Button) findViewById(R.id.create);
+    next.setOnClickListener(new View.OnClickListener(){
+    	public void onClick(View view) {
+    		Intent myIntent = new Intent(view.getContext(), GUI.class);
+    		startActivity(myIntent);
+    	}});
+	}
 }
 /*e
 public void onCreate(Bundle savedInstanceState) {
