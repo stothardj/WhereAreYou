87b95fa2ae5f654a51c42c411181067a1f7f0e92
Adam <xzy1243@gmail.com>
2010-05-25 02:47:17 -0700

    added hong's new code into my current version

--- androidgui/GUI/src/gogodeX/GUI/MapTabActivity.java
+++ androidgui/GUI/src/gogodeX/GUI/MapTabActivity.java
@@ -10,42 +10,47 @@ import com.google.android.maps.*;
 public class MapTabActivity extends MapActivity {
     /** Called when the activity is first created. */
 	private static List<Overlay> mapOverlays;
-	private Location L;
-	private static Drawable drawable;
-	
+	private MapView mapView;
+	//private Location L;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.maptabactivityview);
-	    MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView = (MapView) findViewById(R.id.mapview);
 	    mapView.setBuiltInZoomControls(true);
-	    
+	    mapOverlays = mapView.getOverlays();
+	    Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+
+	    /*
 	    L = GPSUpdater.getLocation();
 	    Double lat = L.getLatitude();
 	    Double lon = L.getLongitude();
 	    int lat1 = (int) (lat * 1000000);
-	    int lon1 = (int) (lon * 1000000);
-	    
-	    mapOverlays = mapView.getOverlays();
-	    drawable = this.getResources().getDrawable(R.drawable.icon);
-	    
-	    BuddyOverlay itemizedoverlay = new BuddyOverlay(drawable);
+	    int lon1 = (int) (lon * 1000000);	    
 	    BuddyOverlay myOverlay = new BuddyOverlay(drawable);
-	    
-	    GeoPoint point = new GeoPoint(19240000,-99120000);
 	    GeoPoint myPoint = new GeoPoint(lat1, lon1);
-	    OverlayItem overlayitem = new OverlayItem(point, "", "");
 	    OverlayItem myOverlayItem = new OverlayItem(myPoint, "", "");
-	    itemizedoverlay.addOverlay(overlayitem);
 	    myOverlay.addOverlay(myOverlayItem);
 	    mapOverlays.add(myOverlay);
-	    mapOverlays.add(itemizedoverlay);
-	    
+		*/
+
+	    displayCoordinate(19240000,-99120000,drawable,"","");
+	    initMyLocation();
 	}
-	
+
+	public void displayCoordinate(int lat, int lon, Drawable drawable, String title, String snippet)
+	{
+		BuddyOverlay itemizedoverlay = new BuddyOverlay(drawable);
+		GeoPoint point = new GeoPoint(lat,lon);
+		itemizedoverlay.addOverlay(new OverlayItem(point, title, snippet));
+		mapOverlays.add(itemizedoverlay);
+	}
+
+	// Function no longer used
 	public static void onLocationChange(Location L)
 	{
-		mapOverlays.remove(0);
+		/*mapOverlays.remove(0);
 	    BuddyOverlay myOverlay = new BuddyOverlay(drawable);
 	    
 	    Double lat = L.getLatitude();
@@ -56,10 +61,19 @@ public class MapTabActivity extends MapActivity {
 	    GeoPoint myPoint = new GeoPoint(lat1, lon1);
 	    OverlayItem myOverlayItem = new OverlayItem(myPoint, "", "");
 	    myOverlay.addOverlay(myOverlayItem);
-	    mapOverlays.add(myOverlay);
-		
+	    mapOverlays.add(myOverlay);*/
+
 	}
-	
+
+	// Sets up the self marker on the map
+	// marker is updated automatically
+	private void initMyLocation()
+	{
+		MyLocationOverlay myLocOverlay = new MyLocationOverlay(this, mapView);
+		myLocOverlay.enableMyLocation();
+		mapView.getOverlays().add(myLocOverlay);
+	}
+
     @Override
     protected boolean isRouteDisplayed() {
         return false;
