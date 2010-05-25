12b2b0e68851932a578c3e42df3647d670b74db8
Brian Garfinkel <bagrm@ucla.edu>
2010-05-24 20:02:57 -0700

    More bugs fixed, janky implementation of runtime updating map

--- androidgui/GUI/src/gogodeX/GUI/MapTabActivity.java
+++ androidgui/GUI/src/gogodeX/GUI/MapTabActivity.java
@@ -9,6 +9,10 @@ import com.google.android.maps.*;
 
 public class MapTabActivity extends MapActivity {
     /** Called when the activity is first created. */
+	private static List<Overlay> mapOverlays;
+	private Location L;
+	private static Drawable drawable;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -16,14 +20,15 @@ public class MapTabActivity extends MapActivity {
 	    MapView mapView = (MapView) findViewById(R.id.mapview);
 	    mapView.setBuiltInZoomControls(true);
 	    
-	    Location L = GPSUpdater.getLocation();
+	    L = GPSUpdater.getLocation();
 	    Double lat = L.getLatitude();
 	    Double lon = L.getLongitude();
 	    int lat1 = (int) (lat * 1000000);
 	    int lon1 = (int) (lon * 1000000);
 	    
-	    List<Overlay> mapOverlays = mapView.getOverlays();
-	    Drawable drawable = this.getResources().getDrawable(R.drawable.icon);
+	    mapOverlays = mapView.getOverlays();
+	    drawable = this.getResources().getDrawable(R.drawable.icon);
+	    
 	    BuddyOverlay itemizedoverlay = new BuddyOverlay(drawable);
 	    BuddyOverlay myOverlay = new BuddyOverlay(drawable);
 	    
@@ -31,12 +36,28 @@ public class MapTabActivity extends MapActivity {
 	    GeoPoint myPoint = new GeoPoint(lat1, lon1);
 	    OverlayItem overlayitem = new OverlayItem(point, "", "");
 	    OverlayItem myOverlayItem = new OverlayItem(myPoint, "", "");
-	    
 	    itemizedoverlay.addOverlay(overlayitem);
 	    myOverlay.addOverlay(myOverlayItem);
-	    mapOverlays.add(itemizedoverlay);
 	    mapOverlays.add(myOverlay);
+	    mapOverlays.add(itemizedoverlay);
 	    
+	}
+	
+	public static void onLocationChange(Location L)
+	{
+		mapOverlays.remove(0);
+	    BuddyOverlay myOverlay = new BuddyOverlay(drawable);
+	    
+	    Double lat = L.getLatitude();
+	    Double lon = L.getLongitude();
+	    int lat1 = (int) (lat * 1000000);
+	    int lon1 = (int) (lon * 1000000);
+	    
+	    GeoPoint myPoint = new GeoPoint(lat1, lon1);
+	    OverlayItem myOverlayItem = new OverlayItem(myPoint, "", "");
+	    myOverlay.addOverlay(myOverlayItem);
+	    mapOverlays.add(myOverlay);
+		
 	}
 	
     @Override
